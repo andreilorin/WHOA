6a66ba8efaad1d03d0cb2018c754f0e968437b03
Lorin <andreilorin@gmail.com>
2018-02-25 22:40:04 +0000

    implement basic rest api

--- Application/src/main.go
+++ Application/src/main.go
@@ -1,16 +1,64 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+var (
+	address = flag.String("addr", ":8080", "http service address")
+	data    map[string]string
 )
 
 func main() {
+	flag.Parse()
+	data = map[string]string{}
+
+	router := httprouter.New()
+
+	router.GET("/entry/:key", show)
+	router.GET("/list", show)
+	router.PUT("/entry/:key/:value", update)
+	router.DELETE("/delete/:key", remove)
+
 	http.HandleFunc("/", runApp)
 	http.HandleFunc("/home", home)
 	http.HandleFunc("/about", about)
 	http.HandleFunc("/static/", serveStatic)
-	http.ListenAndServe(":8080", nil)
+
+	log.Fatal(http.ListenAndServe(*address, router))
+
+}
+
+func show(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	key := params.ByName("key")
+	if key == "" {
+		fmt.Fprintf(writer, "Read list: %v", data)
+		return
+	}
+	fmt.Fprintf(writer, "Read entry: data[%s]= %s", key, data[key])
+}
+
+func update(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	key := params.ByName("key")
+	value := params.ByName("value")
+
+	data[key] = value
+
+	fmt.Fprintf(writer, "Updated: data[%s] = %s", key, data[key])
+}
+
+func remove(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	key := params.ByName("key")
+
+	delete(data, key)
+
+	fmt.Fprintf(writer, "Removed: data[%s]", key)
 }
 
 func runApp(writer http.ResponseWriter, request *http.Request) {
